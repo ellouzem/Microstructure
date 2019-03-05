065ed90f4783066edaa69733fea4d1bf16dc30e8
Sujanth Nagarajah <Sujanth.Nagarajah@grenoble-inp.org>
2019-03-05 14:22:44 +0100

    Estimation gamma1

--- empirique.r
+++ empirique.r
@@ -1,3 +1,26 @@
 library(readr)
 library(dplyr)
-data = read_csv(file = "data_final_2019.csv") 
+library(plm)
+table = read_csv(file = "data_final_2019.csv") 
+
+# subyear = data$year[!is.na(data$pin)]
+
+alpha <- c()
+gamma <- c()
+years = sort(unique(table$year))
+param <- c(6, 8, 10, 12, 14, 16, 18, 20, 22, 27, 28, 29)
+for (y in years)
+{
+  months = sort(unique((table$month[table$year == y])))
+  n = length(months)
+  for (m in months[2:n])
+  {
+    renta = table$return_rf[table$year == y & table$month == m]
+    beta_t1 = table$beta_t1[table$year == y & table$month == m]
+    fit = fpmg(renta ~ beta_t1)
+    alpha <- c(alpha, fit$coefficients[1])
+    gamma <- c(gamma, fit$coefficients[2])
+  }
+}
+
+
